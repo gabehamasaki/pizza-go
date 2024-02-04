35c3009964c71a16b3ad4838294e50b1bed31fd8
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2024-02-04 04:35:55 -0300

    feat: add create order dialog

---
+++ src/app/orders/create-order.tsx
@@ -0,0 +1,90 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from '@/components/ui/dialog'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { PlusCircle } from 'lucide-react'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { Input } from '@/components/ui/input'
+
+export const createOrderSchema = z.object({
+  client: z.string(),
+  value: z.string(),
+})
+
+export default function CreateOrderButton() {
+  const form = useForm<z.infer<typeof createOrderSchema>>({
+    resolver: zodResolver(createOrderSchema),
+  })
+
+  const onSubmit = (data: z.infer<typeof createOrderSchema>) => {
+    console.log(data)
+  }
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="text-center gap-1 font-bold">
+          <PlusCircle className="size-4" />
+          Create order
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create order</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="client"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Client</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="Client" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="value"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Value</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="number" placeholder="Value" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="destructive">Cancel</Button>
+              </DialogClose>
+              <Button type="submit">Create</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -6,30 +6,39 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+import CreateOrderButton from './create-order'
 
 export default function Orders() {
   return (
-    <Table>
-      <TableHeader className="sticky w-full top-0 h-10 border-b-2 border-border rounded-t-md">
-        <TableRow>
-          <TableHead>ID</TableHead>
-          <TableHead>Client</TableHead>
-          <TableHead>Value</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Date</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {Array.from({ length: 32 }).map((_, index) => (
-          <TableRow key={index}>
-            <TableCell>{index + 1}</TableCell>
-            <TableCell>John Doe</TableCell>
-            <TableCell>R$ 100.00</TableCell>
-            <TableCell>Delivered</TableCell>
-            <TableCell>{new Date().toLocaleDateString('pt')}</TableCell>
+    <div className="space-y-8 w-full">
+      <div className="flex items-center justify-between">
+        <h1 className="text-bold text-lg">Orders</h1>
+        <div className="flex">
+          <CreateOrderButton />
+        </div>
+      </div>
+      <Table>
+        <TableHeader className="sticky w-full top-0 h-10 border-b-2 border-border rounded-t-md">
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>Client</TableHead>
+            <TableHead>Value</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Date</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {Array.from({ length: 32 }).map((_, index) => (
+            <TableRow key={index}>
+              <TableCell>{index + 1}</TableCell>
+              <TableCell>John Doe</TableCell>
+              <TableCell>R$ 100.00</TableCell>
+              <TableCell>Delivered</TableCell>
+              <TableCell>{new Date().toLocaleDateString('pt')}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   )
 }
